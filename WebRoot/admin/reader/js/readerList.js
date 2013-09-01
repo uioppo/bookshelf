eb855da8b59dd8101f4f8d0e39d29fe8787c9abf
uioppo <uioppo@163.com>
2013-09-01 13:39:38 +0800

    列表中增加阅读器类型的显示

--- WebRoot/admin/reader/js/readerList.js
+++ WebRoot/admin/reader/js/readerList.js
@@ -25,6 +25,15 @@ function loadReader() {
 						}
 					}
 				},
+				{field:'category',title:'阅读器类型',width:180,align:'center',resizable:true,
+					formatter: function(value,row,index){
+						if (row.category=="01"){
+							return "PC";
+						} else if(row.category=="02"){
+							return "Andirod";
+						}
+					}
+				},
 				{field:'version',title:'版本',width:180,align:'center',resizable:true},
 				{field:'createTime_',title:'创建时间',width:180,align:'center',resizable:true},
 				{field:'operator',title:'操作者',width:200,align:'center',resizable:true},
